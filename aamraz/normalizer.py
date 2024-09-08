e58192331fa7e77df8a0a913107f85c33b853874
mohamad.develop@gmail.com <mohamad.develop@gmail.com>
2024-09-08 21:16:28 +0330

    word2vec model added

--- aamraz/normalizer.py
+++ aamraz/normalizer.py
@@ -11,7 +11,7 @@ class Normalizer:
             'یککیییکیبقویتتبتتتبحاوویتتبتتتبحححچدددددددددررررررررسسسصصطعففففففققکککککگگگگگللللنننننهچهههوووووووووییییییهدرشضغهبببببببححددرسعععففکککممنننلررسححسرحاایییووییحسسکببجطفقلمییرودصگویزعکبپتریفقنااببببپپپپببببتتتتتتتتتتتتففففححححححححچچچچچچچچددددددددژژررککککگگگگگگگگگگگگننننننههههههههههییییءاااووااییییااببببتتتتثثثثججججححححخخخخددذذررززسسسسششششصصصصضضضضططططظظظظععععغغغغففففققققککککللللممممننننههههوویییییییکی"" '
         )
 
-        self.redundant_space_patterns = [
+        self.basic_patterns = [
             (r" {2,}", " "),
             (r"\n{3,}", "\n\n"),
             (r"\u200c{2,}", "\u200c"),
@@ -20,12 +20,17 @@ class Normalizer:
             (r"\b\u200c*\B", ""),
             (r"\B\u200c*\b", ""),
             (r"[ـ\r]", ""),
+            ("[\u064b\u064c\u064d\u064e\u064f\u0650\u0651\u0652]", ""), # diacritics
+            (
+                "[\u0605\u0653\u0654\u0655\u0656\u0657\u0658\u0659\u065a\u065b\u065c\u065d\u065e\u065f\u0670\u0610\u0611\u0612\u0613\u0614\u0615\u0616\u0618\u0619\u061a\u061e\u06d4\u06d6\u06d7\u06d8\u06d9\u06da\u06db\u06dc\u06dd\u06de\u06df\u06e0\u06e1\u06e2\u06e3\u06e4\u06e5\u06e6\u06e7\u06e8\u06e9\u06ea\u06eb\u06ec\u06ed\u06fd\u06fe\u08ad\u08d4\u08d5\u08d6\u08d7\u08d8\u08d9\u08da\u08db\u08dc\u08dd\u08de\u08df\u08e0\u08e1\u08e2\u08e3\u08e4\u08e5\u08e6\u08e7\u08e8\u08e9\u08ea\u08eb\u08ec\u08ed\u08ee\u08ef\u08f0\u08f1\u08f2\u08f3\u08f4\u08f5\u08f6\u08f7\u08f8\u08f9\u08fa\u08fb\u08fc\u08fd\u08fe\u08ff\ufbb2\ufbb3\ufbb4\ufbb5\ufbb6\ufbb7\ufbb8\ufbb9\ufbba\ufbbb\ufbbc\ufbbd\ufbbe\ufbbf\ufbc0\ufbc1\ufc5e\ufc5f\ufc60\ufc61\ufc62\ufc63\ufcf2\ufcf3\ufcf4\ufd3e\ufd3f\ufe70\ufe71\ufe72\ufe76\ufe77\ufe78\ufe79\ufe7a\ufe7b\ufe7c\ufe7d\ufe7e\ufe7f\ufdfa\ufdfb]",
+                "",
+            ), #arabic unicode superscript and subscript characters
         ]
 
     def normalize(self, text):
         normalized_text = text.translate(str.maketrans(self.translation_src, self.translation_dst))
 
-        for pattern, replacement in self.redundant_space_patterns:
+        for pattern, replacement in self.basic_patterns:
             normalized_text = re.sub(pattern, replacement, normalized_text)
 
         return normalized_text
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aamraz',
-    version='0.0.6',
+    version='0.0.7',
     author='Mohammad Mahmoodi Varnamkhasti',
     author_email='research@amzmohammad.com',
     description='This project is a collection of Natural Language Processing tools for Kurdish Language.',
